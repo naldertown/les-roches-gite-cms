7bc95adf64b1bbbdc9dda5a31ad0a21d6bb7e8a5
John Stiles <john@naldertown.com>
2021-08-28 16:42:03 +0100

    Update title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Les Roches Gite - La Roche de Bord - Vanzay - France',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'A beautifully restored gite with great access to the Poitou-Charentes region of France',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
