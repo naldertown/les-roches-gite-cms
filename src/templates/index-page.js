715ce9610d9207b08907babc7755d006f4deedaf
John Stiles <john@naldertown.com>
2021-08-28 16:23:07 +0100

    Update subtitle text

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -74,7 +74,7 @@ export const IndexPageTemplate = ({
                     <h1 className="title">{mainpitch.title}</h1>
                   </div>
                   <div className="tile">
-                    <h3 className="subtitle">{mainpitch.description}</h3>
+                    <h4 className="subtitle">{mainpitch.description}</h4>
                   </div>
                 </div>
                 <div className="columns">
